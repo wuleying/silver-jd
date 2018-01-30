9f43afea292acffd100e57620e01e2359acc66b8
wuleying <5590548@qq.com>
2018-01-30 14:31:57 +0800

    Update

--- jd/jd.go
+++ jd/jd.go
@@ -1,141 +1,129 @@
 package jd
 
 import (
-    "fmt"
-    "github.com/go-clog/clog"
-    "log"
-    "net/http"
-    "strings"
-    "time"
-    "github.com/wuleying/silver-jd/util"
-    "compress/gzip"
-    "io/ioutil"
-    "io"
-)
-
-const (
-    URLSKUState    = "https://c0.3.cn/stocks"
-    URLGoodsDets   = "http://item.jd.com/%s.html"
-    URLGoodsPrice  = "http://p.3.cn/prices/mgets"
-    URLAdd2Cart    = "https://cart.jd.com/gate.action"
-    URLChangeCount = "http://cart.jd.com/changeNum.action"
-    URLCartInfo    = "https://cart.jd.com/cart.action"
-    URLOrderInfo   = "http://trade.jd.com/shopping/order/getOrderInfo.action"
-    URLSubmitOrder = "http://trade.jd.com/shopping/order/submitOrder.action"
-    URLSignIn      = "https://vip.jd.com/common/signin.html"
+	"compress/gzip"
+	"fmt"
+	"github.com/go-clog/clog"
+	"github.com/wuleying/silver-jd/util"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
 )
 
 var (
-    URLForQR = [...]string{
-        "https://passport.jd.com/new/login.aspx",
-        "https://qr.m.jd.com/show",
-        "https://qr.m.jd.com/check",
-        "https://passport.jd.com/uc/qrCodeTicketValidation",
-        "http://home.jd.com/getUserVerifyRight.action",
-    }
+	URLForQR = [...]string{
+		"https://passport.jd.com/new/login.aspx",
+		"https://qr.m.jd.com/show",
+		"https://qr.m.jd.com/check",
+		"https://passport.jd.com/uc/qrCodeTicketValidation",
+		"http://home.jd.com/getUserVerifyRight.action",
+	}
 
-    DefaultHeaders = map[string]string{
-        "User-Agent":      "Chrome/51.0.2704.103",
-        "ContentType":     "application/json",
-        "Connection":      "keep-alive",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "zh-CN,zh;q=0.8",
-    }
+	DefaultHeaders = map[string]string{
+		"User-Agent":      "Chrome/51.0.2704.103",
+		"ContentType":     "application/json",
+		"Connection":      "keep-alive",
+		"Accept-Encoding": "gzip, deflate",
+		"Accept-Language": "zh-CN,zh;q=0.8",
+	}
 
-    maxNameLen   = 40
-    cookieFile   = fmt.Sprintf("%s/%s", util.ROOT_DIR, "cookies/jd.cookies")
-    qrCodeFile   = "cookies/jd.qr"
-    strSeperater = strings.Repeat("+", 60)
+	maxNameLen   = 40
+	cookieFile   = fmt.Sprintf("%s/%s", util.ROOT_DIR, "cookies/jd.cookies")
+	qrCodeFile   = "cookies/jd.qr"
+	strSeperater = strings.Repeat("+", 60)
 )
 
 // JDConfig ...
 type JDConfig struct {
-    Period     time.Duration // refresh period
-    ShipArea   string        // shipping area
-    AutoRush   bool          // continue rush when out of stock
-    AutoSubmit bool          // whether submit the order
+	Period     time.Duration // refresh period
+	ShipArea   string        // shipping area
+	AutoRush   bool          // continue rush when out of stock
+	AutoSubmit bool          // whether submit the order
 }
 
 // SKUInfo ...
 type SKUInfo struct {
-    ID        string
-    Price     string
-    Count     int    // buying count
-    State     string // stock state 33 : on sale, 34 : out of stock
-    StateName string // "现货" / "无货"
-    Name      string
-    Link      string
+	ID        string
+	Price     string
+	Count     int    // buying count
+	State     string // stock state 33 : on sale, 34 : out of stock
+	StateName string // "现货" / "无货"
+	Name      string
+	Link      string
 }
 
 // JingDong wrap jing dong operation
 type JingDong struct {
-    JDConfig
-    client *http.Client
-    jar    *util.SimpleJar
-    token  string
+	JDConfig
+	client *http.Client
+	jar    *util.SimpleJar
+	token  string
 }
 
 func NewJingDong(option JDConfig) *JingDong {
-    jd := &JingDong{
-        JDConfig: option,
-    }
+	jd := &JingDong{
+		JDConfig: option,
+	}
 
-    jd.jar = util.NewSimpleJar(util.JarOption{
-        JarType:  util.JarGob,
-        Filename: cookieFile,
-    })
+	jd.jar = util.NewSimpleJar(util.JarOption{
+		JarType:  util.JarGob,
+		Filename: cookieFile,
+	})
 
-    if err := jd.jar.Load(); err != nil {
-        clog.Error(0, "加载Cookies失败: %s", err)
-        jd.jar.Clean()
-    }
+	if err := jd.jar.Load(); err != nil {
+		clog.Error(0, "加载Cookies失败: %s", err)
+		jd.jar.Clean()
+	}
 
-    jd.client = &http.Client{
-        Timeout: time.Minute,
-        Jar:     jd.jar,
-    }
+	jd.client = &http.Client{
+		Timeout: time.Minute,
+		Jar:     jd.jar,
+	}
 
-    return jd
+	return jd
 }
 
 func (jd *JingDong) Release() {
-    if jd.jar != nil {
-        if err := jd.jar.Persist(); err != nil {
-            log.Panic("Failed to persist cookiejar. error %+v.", err)
-        }
-    }
+	if jd.jar != nil {
+		if err := jd.jar.Persist(); err != nil {
+			log.Panic("Failed to persist cookiejar. error %+v.", err)
+		}
+	}
 }
 
 func (jd *JingDong) getResponse(method, URL string, queryFun func(URL string) string) ([]byte, error) {
-    var (
-        err  error
-        req  *http.Request
-        resp *http.Response
-    )
+	var (
+		err  error
+		req  *http.Request
+		resp *http.Response
+	)
 
-    queryURL := URL
-    if queryFun != nil {
-        queryURL = queryFun(URL)
-    }
+	queryURL := URL
+	if queryFun != nil {
+		queryURL = queryFun(URL)
+	}
 
-    if req, err = http.NewRequest(method, queryURL, nil); err != nil {
-        return nil, err
-    }
-    applyCustomHeader(req, DefaultHeaders)
+	if req, err = http.NewRequest(method, queryURL, nil); err != nil {
+		return nil, err
+	}
+	applyCustomHeader(req, DefaultHeaders)
 
-    if resp, err = jd.client.Do(req); err != nil {
-        return nil, err
-    }
+	if resp, err = jd.client.Do(req); err != nil {
+		return nil, err
+	}
 
-    defer resp.Body.Close()
-    var reader io.Reader
+	defer resp.Body.Close()
+	var reader io.Reader
 
-    switch resp.Header.Get("Content-Encoding") {
-    case "gzip":
-        reader, _ = gzip.NewReader(resp.Body)
-    default:
-        reader = resp.Body
-    }
+	switch resp.Header.Get("Content-Encoding") {
+	case "gzip":
+		reader, _ = gzip.NewReader(resp.Body)
+	default:
+		reader = resp.Body
+	}
 
-    return ioutil.ReadAll(reader)
+	return ioutil.ReadAll(reader)
 }
--- util/const.go
+++ util/const.go
@@ -1,13 +1,26 @@
 package util
 
 import (
-    "time"
+	"time"
+)
+
+// 全局常量
+const (
+	URL_SKU_STATE    = "https://c0.3.cn/stocks"
+	URL_GOODS_DETS   = "http://item.jd.com/%s.html"
+	URL_GOODS_PRICE  = "http://p.3.cn/prices/mgets"
+	URL_ADD_TO_CART  = "https://cart.jd.com/gate.action"
+	URL_CHANGE_COUNT = "http://cart.jd.com/changeNum.action"
+	URL_CART_INFO    = "https://cart.jd.com/cart.action"
+	URL_ORDER_INFO   = "http://trade.jd.com/shopping/order/getOrderInfo.action"
+	URL_SUBMIT_ORDER = "http://trade.jd.com/shopping/order/submitOrder.action"
+	URL_SIGN_IN      = "https://vip.jd.com/common/signin.html"
 )
 
 // 全局变量
 var (
-    // 根目录
-    ROOT_DIR     = FileGetParentDirectory(FileGetCurrentDirectory())
-    // 当前时间
-    CURRENT_TIME = time.Now().String()
+	// 根目录
+	ROOT_DIR = FileGetParentDirectory(FileGetCurrentDirectory())
+	// 当前时间
+	CURRENT_TIME = time.Now().String()
 )
