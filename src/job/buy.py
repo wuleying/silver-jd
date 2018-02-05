559220b8207319541381cd4a4c9629aab60f98f0
wuleying <5590548@qq.com>
2018-02-05 11:15:17 +0800

    Update

--- src/job/buy.py
+++ src/job/buy.py
@@ -6,9 +6,18 @@ from .login import Login
 class Buy(Login):
     job_name = '商品购买'
 
+    # 加入购物车
+    add_cart_url = 'https://cart.jd.com/gate.action?pid=%s&pcount=%s&ptype=1'
+
     def __init__(self, session: Session):
         super().__init__(session)
         self.page_data = ''
 
     def buy(self):
+        self.add_cart()
+
+        return True
+
+    def add_cart(self):
+        r = self.session.get(self.test_url, allow_redirects=False)
         return True
