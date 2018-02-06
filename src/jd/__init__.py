1e176bfc1a28047a5bf322216a6d5c1115cd4fc6
wuleying <5590548@qq.com>
2018-02-06 15:36:37 +0800

    Update

--- src/jd/__init__.py
+++ src/jd/__init__.py
@@ -1,14 +1,25 @@
 import logging
+import sys
 
 logger = logging.getLogger('App.JD')
-formatter = '%(asctime)s %(name)s[%(module)-s] %(levelname)s: %(message)s'
+formatter = logging.Formatter('%(asctime)s %(name)s[%(module)-s] %(levelname)s: %(message)s')
+
 
 def set_logger():
     logger.propagate = False
     logger.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter(formatter))
-    logger.addHandler(handler)
+
+    # 文件日志
+    file_handler = logging.FileHandler("/tmp/silver-jd.log")
+    file_handler.setFormatter(formatter)
+
+    # 控制台日志
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.formatter = formatter
+
+    # 为logger添加的日志处理器
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
 
 
 set_logger()
