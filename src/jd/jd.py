f79c97ef999f1d2718842b229a6c191ab500b6ec
wuleying <5590548@qq.com>
2018-02-07 15:09:20 +0800

    Update

--- src/jd/jd.py
+++ src/jd/jd.py
@@ -1,5 +1,7 @@
 import json
+import random
 import re
+import time
 
 import requests
 from lxml import etree
@@ -11,9 +13,24 @@ class JD(object):
     logger = jd.logger
 
     # 请求URL
+    # 通行证
     passport_url = 'https://passport.jd.com/new/login.aspx'
+    # 账户登录
     login_url = 'https://passport.jd.com/uc/loginService'
+    # 添加到购物车
     add_cart_url = 'https://cart.jd.com/gate.action?pid={}&pcount={}&ptype=1'
+    # 购物车
+    cart_url = 'https://cart.jd.com'
+    # 取消商品
+    cancel_item_url = 'https://cart.jd.com/cancelItem.action?rd={}'
+    # 选择商品
+    select_item_url = 'https://cart.jd.com/selectItem.action?rd={}'
+    # 订单结算信息
+    order_info_url = 'https://trade.jd.com/shopping/order/getOrderInfo.action?rid={}'
+    # 提交订单
+    submit_order_url = 'https://trade.jd.com/shopping/order/submitOrder.action'
+    # stock
+    stock_url = 'http://c0.3.cn/stock?skuId={}&cat=652,829,854&area={}'
 
     passport_headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) '
@@ -32,9 +49,15 @@ class JD(object):
         'X-Requested-With': 'XMLHttpRequest'
     }
 
+    # 地址编码
+    area_code = '1_72_2799_0'
+    # 下单重试时间，单位：秒
+    sleep_time = 60
+
     request_session = ''
 
     track_id = ''
+    e_id = ''
     # 商品ID
     pid = ''
     # 商品数量
@@ -46,14 +69,16 @@ class JD(object):
             self.request_session = requests.Session()
             self.request_session.headers = self.passport_headers
 
-            # 请求登录页面
-            request = self.request_session.get(url=self.passport_url, headers=self.passport_headers)
-            sel = etree.HTML(request.content)
+        # 请求登录页面
+        request = self.request_session.get(url=self.passport_url, headers=self.passport_headers)
+        sel = etree.HTML(request.content)
 
+        self.e_id = sel.xpath('//input[@id="eid"]/@value')[0]
         self.auth_code = 'http:' + sel.xpath('//img[@id="JD_Verification1"]/@src2')[0]
+
         self.params = {
             'uuid': sel.xpath('//input[@id="uuid"]/@value')[0],
-            'eid': sel.xpath('//input[@id="eid"]/@value')[0],
+            'eid': self.e_id,
             '_t': sel.xpath('//input[@id="token"]/@value')[0],
             'loginType': 'c',
             'loginname': username,
@@ -97,4 +122,60 @@ class JD(object):
 
     # 提交订单
     def submit(self):
-        return True
+        self.logger.info('开始尝试下单！')
+
+        # 随机数
+        r = random.random()
+
+        item_data = {
+            'outSkus': '',
+            'pid': self.pid,
+            'ptype': '1',
+            'packId': '0',
+            'targetId': '0',
+            'promoID': '0',
+            'locationId': '1-2810-6501-0'
+        }
+
+        # 取消商品
+        self.request_session.post(self.cancel_item_url.format(str(r)), data=item_data)
+
+        # 勾选商品
+        self.request_session.post(self.select_item_url.format(str(r)), data=item_data)
+
+        submit_data = {
+            'overseaPurchaseCookies': '',
+            'submitOrderParam.sopNotPutInvoice': 'false',
+            'submitOrderParam.trackID': self.track_id[0],
+            'submitOrderParam.ignorePriceChange': '0',
+            'submitOrderParam.btSupport': '0',
+            'submitOrderParam.eid': self.e_id,
+            'submitOrderParam.fp': 'b31fc738113fbc4ea5fed9fc9811acc6',
+        }
+
+        while True:
+            request_stock = self.request_session.get(self.stock_url.format(self.pid, self.area_code))
+            js = json.loads(request_stock.text)
+
+            self.logger.info(js['stock']['StockState'])
+
+            # 33有货 34无货
+            if js['stock']['StockState'] == 33:
+                self.logger.info('库存状态：有货')
+                request_submit = self.request_session.post(self.submit_order_url, data=submit_data)
+                self.logger.info('正在提交订单...')
+                submit_js = json.loads(request_submit.text)
+
+                # 判断是否下单成功
+                if submit_js['success']:
+                    self.logger.info('下单成功! 请速去付款！')
+                    break
+                else:
+                    self.logger.info('下单失败! {}秒后重试。'.format(self.sleep_time))
+                    # 重新尝试下单
+                    time.sleep(self.sleep_time)
+                    continue
+            elif js['stock']['StockState'] != 33:
+                self.logger.info('无货，{}秒后重试。'.format(self.sleep_time))
+                time.sleep(self.sleep_time)
+                continue
